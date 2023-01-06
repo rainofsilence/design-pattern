80fe40b367c8a1b6fe449a87122b9d43672b15ab
rainofsilence <rainofsilecne@foxmail.com>
2023-01-06 22:12:40 +0800

    init

---
+++ src/main/java/cn/rofs/Main.java
@@ -0,0 +1,12 @@
+package cn.rofs;
+
+/**
+ * <em>Created by rainofsilece on $YEAR/$MONTH/$DAY $TIME:$SECOND.</em><br/>
+ *
+ * @desc
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
